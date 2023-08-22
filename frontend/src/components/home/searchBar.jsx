2a2f3c33a40f0d00829deeda8939c194b397fc57
Asher <ashersam116@gmail.com>
2023-08-22 00:46:48 -0900

    UI upgrade for home page

--- frontend/src/components/home/searchBar.jsx
+++ frontend/src/components/home/searchBar.jsx
@@ -1,46 +1,44 @@
 import { Button, InputBase, TextField } from "@mui/material";
 import { width } from "@mui/system";
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 function SearchBar() {
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
 
-    const [inputValue, setInputValue] = useState('');
-    const navigate = useNavigate();
+  function onSearchClick(event) {
+    let data = {
+      value: String(inputValue),
+    };
 
-    function onSearchClick(event) {
-        let data = {
-            value: String(inputValue)
-        }
+    navigate(`/video?code=${inputValue}`);
+  }
 
-        navigate(`/video?code=${inputValue}`);
-        
-    }
-
-    return (
-        <div className="searchBarContainer">
-            <div className="searchBar">
-                <TextField
-                    placeholder="Enter Video Link"
-                    fullWidth
-                    size="small"
-                    value={inputValue}
-                    onChange={event => setInputValue(event.target.value)}
-                    focused={false}
-                />
-                &nbsp;
-                &nbsp;
-                <Button
-                    variant="outlined"
-                    size="large"
-                    endIcon={<SearchOutlinedIcon fontSize="large" />}
-                    onClick={onSearchClick}
-                ><b>Go</b></Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="searchBarContainer">
+      <div className="searchBar">
+        <TextField
+          placeholder="Enter Video Link"
+          fullWidth
+          size="small"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          focused={false}
+        />
+        &nbsp; &nbsp;
+        <Button
+          variant="outlined"
+          size="large"
+          endIcon={<SearchOutlinedIcon fontSize="large" />}
+          onClick={onSearchClick}
+        >
+          <b>Go</b>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
